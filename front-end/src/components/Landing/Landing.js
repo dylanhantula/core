8dbdf82ddb2b3d9d1aadd8a51209341292ff9a11
Dylan Hantula <dylanhantula@Dylans-MacBook-Pro.local>
2020-05-14 11:32:31 -0700

    Meeting commit

--- front-end/src/components/Landing/Landing.js
+++ front-end/src/components/Landing/Landing.js
@@ -3,7 +3,7 @@ import React from "react";
 const Landing = () => {
   return (
     <div>
-      <h1>Welcome to Athletes Untapped</h1>
+      <h1>Welcome to Athletes Untapped. Today is Thursday. Tomorrow is Friday.</h1>
     </div>
   );
 };
